31e2a9bcdb27ef3fa6b7e9af0238e2e1edede981
Myatty <myintmyatps664@gmail.com>
2025-05-08 11:38:07 +0630

    update: configure to nginx and build image, ready to use for ci cd

---
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -0,0 +1,80 @@
+import { PRIORITIES, PRIORITY_DEFAULT } from "../../constants/priorities";
+import { TodoFormFields } from "../TodoFormFields/TodoFormFields";
+import styles from "./TodoListItem.module.css";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { getTodoSchema } from "../../Schemas/todo";
+
+export function TodoListItem({ todo, onUpdate, onDelete }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(getTodoSchema()), defaultValues: todo });
+
+  function handleCompleted(event) {
+    onUpdate(todo.id, { ...todo, completed: event.target.checked });
+  }
+
+  function handleEdit(data) {
+    onUpdate(todo.id, data);
+    setIsEditing(false);
+  }
+
+  const viewingTemplate = (
+    <div className={styles.Content}>
+      <input
+        type="checkbox"
+        name="completed"
+        checked={todo.completed}
+        onChange={handleCompleted}
+        className={styles.Status}
+      />
+
+      <div className={styles.Info}>
+        {todo.name}
+
+        {todo.description && (
+          <span className={styles.Description}>{todo.description}</span>
+        )}
+
+        <div className={styles.AdditionalInfo}>
+          {todo.deadline}{" "}
+          {todo.priority !== PRIORITY_DEFAULT && (
+            <span style={{ color: PRIORITIES[todo.priority].color }}>
+              {PRIORITIES[todo.priority].label}
+            </span>
+          )}
+        </div>
+      </div>
+
+      <div className={styles.Controls}>
+        <button onClick={() => setIsEditing(true)}>✎</button>
+        <button onClick={() => onDelete(todo.id)}>⌫</button>
+      </div>
+    </div>
+  );
+
+  const editingTemplate = (
+    <form
+      className={styles.Content}
+      onReset={() => setIsEditing(false)}
+      onSubmit={handleSubmit(handleEdit)}
+    >
+      <TodoFormFields todo={todo} register={register} errors={errors} />
+
+      <div className={styles.Controls}>
+        <input type="submit" value="💾" />
+        <input type="reset" value="❌" />
+      </div>
+    </form>
+  );
+
+  return (
+    <li className={styles.TodoListItem} data-completed={todo.completed}>
+      {isEditing ? editingTemplate : viewingTemplate}
+    </li>
+  );
+}
